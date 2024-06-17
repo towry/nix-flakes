bbb2b07c8363d55d9797bca519cf3402f005e91f
Towry Wang <towry@users.noreply.github.com>
2024-06-18 00:30:13 +0800

    upgrade zellij, fix the wasm issues

--- zellij/flake.nix
+++ zellij/flake.nix
@@ -16,16 +16,16 @@
                 pname = "zellij";
                 version = "0.41.0";
                 src = fetchFromGitHub {
-                  owner = "zellij-org";
+                  owner = "towry";
                   repo = "zellij";
-                  rev = "91cf59383672a797414ed5b68a740a6df689ac05";
-                  hash = "sha256-oV4dNkyN72SMtaljizF1EscoO/a/1GllQ7Cgz04w5Hc=";
+                  rev = "910d657f4e93a320a814e17b5d717b088d86c417";
+                  hash = "sha256-pcrNsqPj17gOp6brFC6pSyUL0rGqtaWHScbfTSGG1l0=";
                 };
                 cargoDeps = zellij.cargoDeps.overrideAttrs (
                   lib.const {
                     name = "${pname}-vendor.tar.gz";
                     inherit src;
-                    outputHash = "sha256-KkAU797oq7AFpKqGgyISqy+6CdM/yoFjRZjhwLJsXYs=";
+                    outputHash = "sha256-fVXt/9ygm4vJ0XfLl5o0/Ki4mvAkq9f4PtZjxzgny1s=";
                   }
                 );
                 buildInputs =
